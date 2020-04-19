9e688226c696e50f06bbcfb4e314571aa4038cb6
Benjamin Vertonghen <benjamin.vertonghen@apped.be>
2020-04-19 17:59:21 +0200

    Clean-up V2

--- scripts/notetoselfV2.js
+++ scripts/notetoselfV2.js
@@ -5,22 +5,11 @@ class Sticky {
     this.color = color;
   }
 
-  get id() {
-    return this._id;
-  }
-  get note() {
-    return this._note;
-  }
-  get color() {
-    return this._color;
-  }
-
-  set note(value) {
-    this._note = value;
-  }
-  set color(value) {
-    this._color = value;
-  }
+  get id() { return this._id;}
+  get note() {return this._note;}
+  get color() {return this._color;}
+  set note(value) {this._note = value;}
+  set color(value) {this._color = value;}
 }
 
 class StickiesComponent {
@@ -33,17 +22,21 @@ class StickiesComponent {
   }
 
   toHTML() {
-    const allStickies = Object.entries(this._storage).reduce(
-      (result, [key, value]) => {
-        // converteert JSON string naar object literal
-        const storObj = JSON.parse(value);
-        // converteert object literal naar object van class Sticky
-        const sticky = new Sticky(storObj._note, storObj._color);
-        return (result += `${key}:${sticky.note}-${sticky.color}\n`);
-      }, '');
+    const allStickies = 
+        Object.entries(this._storage)
+        .reduce((result, [key, value]) => 
+        {
+          // converteert JSON string naar object literal
+          const storObj = JSON.parse(value);
+          // converteert object literal naar object van class Sticky
+          const sticky = new Sticky(storObj._note, storObj._color);
+          return (result += '${key}:${sticky.note}-${sticky.color}\n');
+        }, '');
     alert(allStickies);
   }
-
+  clear() {
+    this._storage.clear();
+  }
   addSticky(note, color) {
     const sticky = new Sticky(note, color);
     this._storage.setItem(sticky.id, JSON.stringify(sticky));
@@ -51,15 +44,14 @@ class StickiesComponent {
 }
 
 function init() {
-  const stickiesComponent = new StickiesComponent(window.localStorage);
+  const stickiesComponent = new StickiesComponent(localStorage);
   const addButton = document.getElementById('add');
   const clearButton = document.getElementById('clear');
   const noteText = document.getElementById('notetext');
   const noteColor = document.getElementById('notecolor');
 
   if (!stickiesComponent.storage) {
-    //browser ondersteunt geen storage
-    alert('no storage available. ');
+    alert('browser ondersteunt geen storage');
     addButton.disabled = true;
     clearButton.disabled = true;
     return;
@@ -67,8 +59,13 @@ function init() {
 
   addButton.onclick = function () {
     stickiesComponent.addSticky(noteText.value, noteColor.value);
+    noteText.value = '';
     stickiesComponent.toHTML();
   };
+  clearButton.onclick = function () {
+    stickiesComponent.clear();
+  };
+
 }
 
 window.onload = init;
