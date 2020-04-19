e001af82ffe99cc1f14550e32868ee41d50607ea
Benjamin Vertonghen <benjamin.vertonghen@apped.be>
2020-04-19 17:59:12 +0200

    Clean-up V1

--- scripts/notetoselfV1.js
+++ scripts/notetoselfV1.js
@@ -2,17 +2,18 @@ class StickiesComponent {
   constructor(storage) {
     this._storage = storage;
   }
-
   get storage() {
     return this._storage;
   }
-
   toHTML() {
-    const allStickies = Object.entries(this.storage).reduce(
-      (result, [key, value]) => result += `${key}: ${value}\n`, '');
+    const allStickies = Object.entries(this._storage)
+                              .reduce((result, [key, value]) => 
+                               result += `${key}: ${value}\n`, '');
     alert(allStickies);
   }
-
+  clear() {
+    this._storage.clear();
+  }
   addSticky(note) {
     const key = 'id-' + Math.random().toString(36).substr(2);
     this._storage.setItem(key, note);
@@ -20,22 +21,24 @@ class StickiesComponent {
 }
 
 function init() {
-  const stickiesComponent = new StickiesComponent(window.localStorage);
+  const stickiesComponent = new StickiesComponent(localStorage);
   const addButton = document.getElementById('add');
   const clearButton = document.getElementById('clear');
+  const noteText = document.getElementById('notetext');
 
   if (!stickiesComponent.storage) {
-    //browser ondersteunt geen storage
-    alert('no storage available. ');
+    alert('browser ondersteunt geen storage');
     addButton.disabled = true;
     clearButton.disabled = true;
     return;
   }
-
   addButton.onclick = function () {
-    const noteText = document.getElementById('notetext');
     stickiesComponent.addSticky(noteText.value);
+    noteText.value = '';
     stickiesComponent.toHTML();
+  };
+  clearButton.onclick = function () {
+    stickiesComponent.clear();
   };
 }
 
